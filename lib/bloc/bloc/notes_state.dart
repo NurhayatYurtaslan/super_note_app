f10da85dad6317518050820bb478698d8a472295
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-07 00:33:26 +0300

    add ✨: notes state file

---
+++ lib/bloc/bloc/notes_state.dart
@@ -0,0 +1,39 @@
+part of 'notes_bloc.dart';
+
+abstract class NoteState {}
+
+class NoteInitialState extends NoteState {}
+
+class NoteLoadingState extends NoteState {}
+
+class NoteLoadedState extends NoteState {
+  NoteLoadedState(this.notes);
+  List<Note> notes;
+}
+
+class AllNoteState extends NoteState {}
+
+class DeleteNoteState extends NoteState {
+  DeleteNoteState(this.notes);
+  List<Note> notes;
+}
+class CreateNoteState extends NoteState {
+  CreateNoteState(this.notes);
+
+  List<Note> notes;
+}
+
+class UpdateNoteState extends NoteState {
+  UpdateNoteState(this.notes);
+  List<Note> notes;
+}
+
+class ErrorNoteState extends NoteState {
+  ErrorNoteState(this.message);
+  String message;
+}
+
+class ShowNoteInViewState extends NoteState {
+  ShowNoteInViewState(this.inGrid);
+  bool inGrid = true;
+}
