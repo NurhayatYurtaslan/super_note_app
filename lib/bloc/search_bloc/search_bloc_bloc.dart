e23f40ca37b3dd570dd9d90c16d60371bfa820e0
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-08 23:16:28 +0300

    update 🆕: search bloc file

--- lib/bloc/search_bloc/search_bloc_bloc.dart
+++ lib/bloc/search_bloc/search_bloc_bloc.dart
@@ -1,13 +1,35 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:super_note_app/models/note.dart';
+import 'package:super_note_app/repository/repository.dart';
 
 part 'search_bloc_event.dart';
 part 'search_bloc_state.dart';
 
-class SearchBlocBloc extends Bloc<SearchBlocEvent, SearchBlocState> {
-  SearchBlocBloc() : super(SearchBlocInitial()) {
-    on<SearchBlocEvent>((event, emit) {
-      // TODO: implement event handler
+class SearchBloc extends Bloc<SearchEvent, SearchState> {
+  final NoteRepository _notesRepository = NoteRepository();
+  SearchBloc() : super(SearchInitialState()) {
+    on<SearchGetAllNotesEvent>((event, emit) async {
+      emit.call(SearchLoadingListState());
+      try {
+        List<Note> notes = await _notesRepository.getAllNotes();
+        emit.call(SearchLoadedListState(notes));
+      } catch (e) {
+        emit.call(SearchErrorState(e.toString()));
+      }
+    });
+
+    on<SearchGetNotesWithTextEvent>((event, emit) {
+      List<Note> notesList = event.notesList;
+      List<Note> filteredList = [];
+      filteredList = notesList
+          .where((note) =>
+              note.title!.toLowerCase().contains(event.text.toLowerCase()))
+          .toList();
+      if (event.text.isEmpty) {
+        emit.call(SearchGetNotesWithTextState(notesList));
+      } else {
+        emit.call(SearchGetNotesWithTextState(filteredList));
+      }
     });
   }
 }
