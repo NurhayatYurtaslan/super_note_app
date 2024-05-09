5832c47bf3a637af10fdeb933ce06989f693ea17
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-09 10:37:45 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Notes Keeper',
+      title: 'Super Note App',
       theme: notesTheme,
       debugShowCheckedModeBanner: false,
       home: const HomeScreen(),
